0436fae6928614053009f35861df62b59269aaa7
geeraymer130 <geeraymer130@gmail.com>
2025-03-15 17:08:27 +0000

    Add 9.go

---
+++ src/9.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+func add(a int, b int) {
+fmt.Println(a+b)
+}
+func subtract(a int, b int) {
+fmt.Println(a-b)
+}
+func multiply(a int, b int) {
+fmt.Println(a*b)
+}
+func divide(a int, b int) {
+fmt.Println(a/b)
+}
+func main() {
+add(4,2)
+subtract(4,2)
+multiply(4,2)
+divide(4,2)
+}
