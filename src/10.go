11fa97b94ceb8d01ec916a56704b6ae5b988c886
geeraymer130 <geeraymer130@gmail.com>
2025-03-16 10:14:46 +0000

    Add 10.go

---
+++ src/10.go
@@ -0,0 +1,22 @@
+import "fmt"
+
+func main() {
+	a := 5
+	b := 3
+	c := a + b
+	d := a - b
+	e := a * b
+	f := a / b
+	g := a % b
+	h := a ** b
+	i := a ^ b
+	fmt.Println("a =", a)
+	fmt.Println("b =", b)
+	fmt.Println("c =", c)
+	fmt.Println("d =", d)
+	fmt.Println("e =", e)
+	fmt.Println("f =", f)
+	fmt.Println("g =", g)
+	fmt.Println("h =", h)
+	fmt.Println("i =", i)
+}
