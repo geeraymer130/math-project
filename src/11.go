3c740b8754deab7742ba92cff23b1438c52d2f82
geeraymer130 <geeraymer130@gmail.com>
2025-03-17 20:20:01 +0000

    Add 11.go

---
+++ src/11.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 4
+	b := 5
+	c := a + b
+	d := a * b
+	e := a - b
+	f := a / b
+	g := a % b
+	h := a == b
+	i := a != b
+	j := a > b
+	k := a < b
+	l := a >= b
+	m := a <= b
+	n := a && b
+	o := a || b
+	p := a ^ b
+	q := a | b
+	r := a & b
+	s := a << 2
+	t := a >> 2
+	u := -a
+	v := !a
+	w := a + 1
+	x := a - 1
+	y := a * 10
+	z := a / 10
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+	fmt.Println(m)
+	fmt.Println(n)
+	fmt.Println(o)
+	fmt.Println(p)
+	fmt.Println(q)
+	fmt.Println(r)
+	fmt.Println(s)
+	fmt.Println(t)
+	fmt.Println(u)
+	fmt.Println(v)
+	fmt.Println(w)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
