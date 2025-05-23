8f6ce078ae8d00802fe2221eb5a80a593baa2bc7
geeraymer130 <geeraymer130@gmail.com>
2025-05-23 06:02:17 +0000

    Add 40.go

---
+++ src/40.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Define variables
+    x := 3
+    y := 4
+
+    // Perform arithmetic operations
+    result := x + y
+
+    // Print the result
+    fmt.Println(result)
+}
